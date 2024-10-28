2a0a70c55229fa6b3d744cc3cad94a9148ecf9d6
Ernestools <naysemachoura@yahoo.com>
2024-10-28 06:51:28 +0100

    FewChanges

--- web_app.py
+++ web_app.py
@@ -14,26 +14,12 @@ def model_prediction(test_image):
 
 #Sidebar
 st.sidebar.title("Dashboard")
-app_mode = st.sidebar.selectbox("Select Page",["Home","About Project","Prediction"])
+app_mode = st.sidebar.selectbox("Select Page",["Home","Prediction"])
 
 #Main Page
 if(app_mode=="Home"):
     st.header("FRUITS & VEGETABLES RECOGNITION SYSTEM")
-    #image_path = "home_img.jpg"
-    #st.image(image_path)
-
-#About Project
-elif(app_mode=="About Project"):
-    st.header("About Project")
-    st.subheader("About Dataset")
-    st.text("This dataset contains images of the following food items:")
-    st.code("fruits- banana, apple, pear, grapes, orange, kiwi, watermelon, pomegranate, pineapple, mango.")
-    st.code("vegetables- cucumber, carrot, capsicum, onion, potato, lemon, tomato, raddish, beetroot, cabbage, lettuce, spinach, soy bean, cauliflower, bell pepper, chilli pepper, turnip, corn, sweetcorn, sweet potato, paprika, jalepeño, ginger, garlic, peas, eggplant.")
-    st.subheader("Content")
-    st.text("This dataset contains three folders:")
-    st.text("1. train (100 images each)")
-    st.text("2. test (10 images each)")
-    st.text("3. validation (10 images each)")
+    
 
 #Prediction Page
 elif(app_mode=="Prediction"):
@@ -43,7 +29,7 @@ elif(app_mode=="Prediction"):
         st.image(test_image,width=4,use_column_width=True)
     #Predict button
     if(st.button("Predict")):
-        st.snow()
+        #st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
         #Reading Labels
